ececfc93014ea23d5df1ce7203bee8192e48f8a2
Radia <drineradiaa@gmail.com>
2026-07-16 09:28:31 +0200

    script done

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ docker compose exec php sh -c "php bin/console cache:clear"
 
 docker run --rm -v ./:/var/www/html -w /var/www/html node:22 sh -c "yarn install && yarn build"
 
-docker compose exec php sh -c "chown -R www-data:www-data var"
-docker compose exec php sh -c "chown -R www-data:www-data public"
-docker compose exec php sh -c "chown -R 1000:1000 public/index.php"
+docker compose exec php sh -c "chown -R www-data:www-data var && chown -R www-data:www-data public/build"
 
-docker compose -f composer.prod.yaml up -d
+docker compose -f compose.prod.yaml up -d
